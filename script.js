84cc333bb46c21372511e29aefab035a2fe313be
hannarubensson <hannarubensson@gmail.com>
2024-07-14 21:30:46 +0200

    first draft

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ document.getElementById('email-btn').addEventListener('click', function() {
         this.innerHTML = 'E-mail me';
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let buttons = document.getElementsByClassName('dropdown-btn');
+    
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener('click', function () {
+        let content = this.nextElementSibling;
+        
+        if (content.classList.contains('show')) {
+          content.classList.remove('show');
+        } else {
+          let dropdowns = document.getElementsByClassName('dropdown-content');
+          for (let j = 0; j < dropdowns.length; j++) {
+            dropdowns[j].classList.remove('show');
+          }
+          
+          content.classList.add('show');
+        }
+      });
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const emailText = document.querySelector('.email-text');
+    const email = emailText.getAttribute('data-text');
+    emailText.innerText = email;
+});
